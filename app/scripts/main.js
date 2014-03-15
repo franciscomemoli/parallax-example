3ca6441c77fd2809ef12bfd9fe0ebc68c23655d2
Manuel Olmos Gil <manuolmos88@gmail.com>
2014-03-15 00:52:50 -0300

    I took the attributes from the html and passed when instantiate parralax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,13 @@
 var scene = document.getElementById('scene');
-var parallax = new Parallax(scene);
+var parallax = new Parallax(scene, {
+  calibrateX: false,
+  calibrateY: true,
+  invertX: false,
+  invertY: false,
+  limitX: 70,
+  limitY: 30,
+  scalarX: 60,
+  scalarY: 60,
+  frictionX: 0.1,
+  frictionY: 0.1
+});
